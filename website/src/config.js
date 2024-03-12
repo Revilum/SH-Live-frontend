8149c732b9d6964f1aedc9bd601f4f2ae11adbf6
revilum <revilum.hi@gmail.com>
2024-03-12 19:08:50 +0100

    fixed some sync

--- website/src/config.js
+++ website/src/config.js
@@ -1,5 +1,30 @@
-const WEBSOCKET_URL = "ws://localhost:1934" // change me or run local backend instance
+const WEBSOCKET_URL = new URL("ws://localhost:1934") // change me or run local backend instance
 
-const API_URL = "http://localhost:1825" // change me or run local backend instance
+const API_URL = new URL("http://localhost:1825") // change me or run local backend instance
 
-export {WEBSOCKET_URL, API_URL}
+
+// leaflet marker options
+
+const LINE_OPTIONS = {
+    color: "blue",
+    weight: 4,
+    opacity: 1
+}
+
+const trainIcon = L.icon({
+    iconUrl: "./img/train.png",
+    iconSize:     [32, 32],
+    iconAnchor:   [16, 16],
+    shadowAnchor: [4, 62],
+    popupAnchor:  [0, -16]
+});
+
+const TRAIN_OPTIONS = {
+    icon: trainIcon
+}
+
+// time format
+
+const TIME_FORMAT = {hour: '2-digit', minute:'2-digit'}
+
+export {WEBSOCKET_URL, API_URL, LINE_OPTIONS, TIME_FORMAT, TRAIN_OPTIONS}
--- website/src/index.js
+++ website/src/index.js
@@ -1,7 +1,7 @@
-import {WEBSOCKET_URL, API_URL} from "./config.js";
+import "../../node_modules/leaflet/dist/leaflet.js"
+import * as conf from "./config.js";
 
-let ACTIVE_MARKER
-
+let ACTIVE_MARKER = undefined
 const MARKERS = {}
 
 const map = L.map('map', {
@@ -9,83 +9,46 @@ const map = L.map('map', {
     zoom: 17
 })
 
-const trainIcon = L.icon({
-    iconUrl: './img/train.png',
-    iconSize:     [32, 32],
-    iconAnchor:   [16, 16],
-    shadowAnchor: [4, 62],
-    popupAnchor:  [0, -16]
-});
-
-
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-map.on("click", () => clearMarker(ACTIVE_MARKER))
+map.on("click", async () => await deselectMarker(ACTIVE_MARKER))
 
-const ws = new WebSocket(WEBSOCKET_URL)
+const ws = new WebSocket(conf.WEBSOCKET_URL)
 
-ws.onmessage = (event) => {
-    let data = JSON.parse(event.data)
+ws.onmessage = async (event) => {
+    const data = JSON.parse(event.data)
     const currentUpdateTime = Date.now().toString()
     data.movements.forEach((train) => {
-        if (MARKERS[train.tripId] !== undefined) {
-            MARKERS[train.tripId].marker.setLatLng(new L.LatLng(train.location.latitude, train.location.longitude))
-            MARKERS[train.tripId].lastUpdate = currentUpdateTime
-        } else {
-            const newMarker = L.marker([train.location.latitude, train.location.longitude], {icon: trainIcon})
+        if (MARKERS[train.tripId] !== undefined) { // train is already present on the map
+            updateTrain(train, currentUpdateTime) // update position of the train on the map
+        } else { // train is new and needs to be added to the map
+            const newMarker = L.marker([train.location.latitude, train.location.longitude], conf.TRAIN_OPTIONS)
                 .bindPopup(train.line.name + " nach " + train.direction, {autoPan: false})
                 .on("click", async () => { // get the current trip.
-                    clearMarker(ACTIVE_MARKER)
-                    ACTIVE_MARKER = train.tripId
-                    let response = await fetch(API_URL + "/trip", {
-                        method: "POST",
-                        body: JSON.stringify({id: train.tripId}),
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
+                    MARKERS[train.tripId].layers = new Promise(async (resolve, _) => {
+                        const oldActiveMarker = ACTIVE_MARKER
+                        ACTIVE_MARKER = train.tripId
+                        await deselectMarker(oldActiveMarker)
+                        let trip = (await fetchPOST("trip", {id: train.tripId})).trip
+                        // add station markers
+                        const newMarkers = (trip.stopovers.map((stopover) =>
+                            L.marker([stopover.stop.location.latitude, stopover.stop.location.longitude])
+                                .bindPopup(getStationTextFromStopover(stopover, trip.line.product))
+                                .addTo(map)
+                        ))
+
+                        newMarkers.push(getPolylineFromTrip(trip)) // draw train path to map and add it to the markers
+                        resolve(newMarkers)
+                        // setTimeout(() => resolve(newMarkers), 10)
                     })
-                    let trip = (await response.json()).trip
-                    // draw path to map
-                    let line = trip.polyline.features.map((x) => x.geometry.coordinates.reverse())
-                    MARKERS[train.tripId].layers.push(L.polyline(line, {
-                        color: "blue",
-                        weight: 4,
-                        opacity: 1
-                    }).addTo(map))
-                    // add stations with current departure
-                    const newMarkers = trip.stopovers.map((stopover) => {
-                        let stationText = `<b>${stopover.stop.name}</b>`
-                        if (stopover.arrival !== null) {
-                            stationText += `<br>Ankunft: ${(new Date(stopover.arrival)).toLocaleTimeString("de-DE", {hour: '2-digit', minute:'2-digit'})}`
-                        }
-                        if (stopover.departure !== null) {
-                            stationText += `<br>Abfahrt: ${(new Date(stopover.departure)).toLocaleTimeString("de-DE", {hour: '2-digit', minute:'2-digit'})}`
-                        }
-                        if (stopover.remarks !== undefined) {
-                            stopover.remarks.forEach((remark) => {
-                                if (remark.type === "warning" && remark.products[trip.line.product]) {
-                                    stationText += `<br>${remark.text}`
-                                }
-                            })
-                        }
-
-
-                        return L.marker([stopover.stop.location.latitude, stopover.stop.location.longitude])
-                            .bindPopup(stationText)
-                            .addTo(map)
-                    })
-                    MARKERS[train.tripId].layers = MARKERS[train.tripId].layers.concat(newMarkers)
-
-
                 }).addTo(map)
-            // save active markers
+            // store new train
             MARKERS[train.tripId] = {
                 marker: newMarker,
-                layers: [],
+                layers: new Promise((resolve, _) => resolve([])),
                 lastUpdate: currentUpdateTime
             }
         }
@@ -93,19 +56,60 @@ ws.onmessage = (event) => {
     // remove all trains that were not present since the last update => train has finished its route
     for (const [trip, marker] of Object.entries(MARKERS)) {
         if (marker.lastUpdate !== currentUpdateTime) {
-            clearMarker(trip)
+            await deselectMarker(trip)
             map.removeLayer(marker.marker)
             delete MARKERS[trip]
         }
     }
 }
 
-function clearMarker(cMarker) {
-    if (cMarker !== undefined) {
-        MARKERS[cMarker].layers.forEach((m) => map.removeLayer(m))
+async function deselectMarker(cMarker) {
+    if (cMarker !== undefined) { // train hasn't been removed already or no train is selected
+        (await MARKERS[cMarker].layers).forEach((m) => map.removeLayer(m))
         MARKERS[cMarker].layers = []
         if (cMarker === ACTIVE_MARKER) {
             ACTIVE_MARKER = undefined
         }
     }
 }
+
+function updateTrain(train, updateTime) {
+    MARKERS[train.tripId].marker.setLatLng(new L.LatLng(train.location.latitude, train.location.longitude))
+    MARKERS[train.tripId].lastUpdate = updateTime
+}
+
+async function fetchPOST(path, data) {
+    const endpoint = new URL(conf.API_URL)
+    endpoint.pathname = path
+    const response = await fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    return await response.json()
+    // simulate shitty internet
+    //return new Promise((r, _) => setTimeout(async () => r(await response.json()), 3000))
+}
+
+function getStationTextFromStopover(station, product) {
+    let stationText = `<b>${station.stop.name}</b>`
+    if (station.arrival !== null) {
+        stationText += `<br>Ankunft: ${(new Date(station.arrival)).toLocaleTimeString("de-DE", conf.TIME_FORMAT)}`
+    }
+    if (station.departure !== null) {
+        stationText += `<br>Abfahrt: ${(new Date(station.departure)).toLocaleTimeString("de-DE", conf.TIME_FORMAT)}`
+    }
+    if (station.remarks !== undefined) {
+        station.remarks.forEach((remark) => {
+            if (remark.type === "warning" && remark.products[product]) {
+                stationText += `<br>${remark.text}`
+            }
+        })
+    }
+    return stationText
+}
+
+const getPolylineFromTrip = (trip) =>
+    L.polyline(trip.polyline.features.map((x) => x.geometry.coordinates.reverse()), conf.LINE_OPTIONS).addTo(map)
